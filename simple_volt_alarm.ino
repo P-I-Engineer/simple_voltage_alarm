8ad1a4864ec6b1ea32c8e1d7d0374242fdf7ce9d
P-I-Engineer <jarad.berkman@gmail.com>
2021-10-10 17:09:10 -0700

    changed to defines

--- simple_volt_alarm.ino
+++ simple_volt_alarm.ino
@@ -1,20 +1,20 @@
 
-const int ledPin = 7;
-const int sensorPin = A3;
-const float R1 = 4700.0;
-const float R2 = 2700.0;
-const float WarnVoltage = 3.15;
-const float CritVoltage = 3.05;
-const float resolution = 1023.0;
-const float cells = 2.0;
-const float v_offset = 0.8;
+#define ledPin 7
+#define sensorPin A3
+#define R1 4700.0
+#define R2 2700.0
+#define WarnVoltage 3.15
+#define CritVoltage 3.05
+#define resolution 1023.0
+#define cells 2.0
+#define v_offset 0.5
 
 
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin 1 as an output.
   pinMode(ledPin, OUTPUT);
-  pinMode(sensorPin, INPUT);
+  //pinMode(sensorPin, INPUT);
 
   Serial.begin(9600);
 }
@@ -22,8 +22,8 @@ void setup() {
 // the loop function runs over and over again forever
 void loop() {
   
-  float CellsensorValue = (analogRead(sensorPin) / resolution * 5.0 * (R1+R2) / R2 - v_offset); // / cells; // take read value and convert to average cell value.
-
+  float CellsensorValue = (analogRead(sensorPin) / resolution * 5.0 * (R1+R2) / R2 - v_offset) / cells; // take read value and convert to average cell value.
+  float fullVolt = CellsensorValue *2;
    
   if (CellsensorValue > WarnVoltage){    // if cell value above warnvoltage, keep led on
         digitalWrite(ledPin, HIGH);    
@@ -44,6 +44,8 @@ void loop() {
         delay(200); 
     }
 
-  Serial.println(CellsensorValue);
+  Serial.println(CellsensorValue + String("\t") + fullVolt);
+  //Serial.print("\t");
+  //Serial.println(fullVolt);
   delay(1); 
 }
