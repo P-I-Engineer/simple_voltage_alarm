f38bdbbbe7786fb4ba6dcbbba0ada72d0c899936
p-i-engineer <jarad.berkman@gmail.com>
2022-01-19 17:20:43 -0800

    convert to millis add buzzer

--- simple_volt_alarm.ino
+++ simple_volt_alarm.ino
@@ -15,43 +15,79 @@ start with v_scaler = 1, then compute the percentage your off from your input.
 #define resolution 1023.0 // 10 bit resolution of 5v arduino
 #define cells 2.0         // number of cells in your pack
 #define v_scaler .96      // scale factor for fine tuning.
-
+#define buzpin 9          // buzzer output
+#define led_short 200     // led flash every .2 seconds
+#define led_long 1000     // led flash every second
+#define buz_short 60000   // buzzer every 1 minute
+#define buz_long 180000   // buzzer every 3 minutes
+unsigned long led_time_now = 0;
+unsigned long buzz_time_now = 0;
+unsigned long previousMillis_led = 0;
+unsigned long previousMillis_buzz = 0;
+int ledState = LOW;
 
 // the setup function runs once when you press reset or power the board
 void setup() {
 
   pinMode(ledPin, OUTPUT);       // sets led pin to output
   pinMode(sensorPin, INPUT);     // sets sensor pin to input
+  pinMode(buzpin, OUTPUT);       //sets buzzer pin to output
 
-  Serial.begin(9600);  // for Debug
-}
+  }
 
 // the loop function runs over and over again forever
 void loop() {
   
   float CellsensorValue = (analogRead(sensorPin) / resolution * 5.0 * (R1+R2) / R2 * v_scaler) / cells; // take read value and convert to average cell value.
-  float fullVolt = CellsensorValue *2;  // for debug
-  int raw = analogRead(sensorPin);      // for debug 
+  
+  unsigned long currentMillis_led = millis();
+  unsigned long currentMillis_buzz = millis();
+  
    
   if (CellsensorValue > WarnVoltage){    // if cell value above WarnVoltage, keep led on
-        digitalWrite(ledPin, HIGH);    
-                      
-    }
+        
+        digitalWrite(ledPin, HIGH);   
+  }
 
     else if (CellsensorValue < WarnVoltage && CellsensorValue > CritVoltage){   // if cell value below warn and above critical slow flash
-        digitalWrite(ledPin, HIGH);   
-        delay(1000);                      
-        digitalWrite(ledPin, LOW);    
-        delay(1000); 
-    }
+        
+        if (currentMillis_led - previousMillis_led >= led_long) {
+  
+            previousMillis_led = currentMillis_led;
+    
+        if (ledState == LOW) {
+          ledState = HIGH;
+        } else {
+         ledState = LOW;
+            }
+
+        digitalWrite(ledPin, ledState);
+          } 
+
+        if (currentMillis_buzz - previousMillis_buzz >= buz_long) {
+            previousMillis_buzz = currentMillis_buzz;
+            tone(9,200,1000);
+          }          
+        }
 
     else {                        // if cell value below critical voltage fast flash
-        digitalWrite(ledPin, HIGH);   
-        delay(200);                       
-        digitalWrite(ledPin, LOW);    
-        delay(200); 
-    }
+        
+        if (currentMillis_led - previousMillis_led >= led_short) {
+  
+        previousMillis_led = currentMillis_led;
+    
+        if (ledState == LOW) {
+            ledState = HIGH;
+        } else {
+         ledState = LOW;
+        }
+          digitalWrite(ledPin, ledState);
+        } 
 
-  Serial.println(CellsensorValue + String("\t") + fullVolt + String("\t") + raw);   // for debug
-  delay(1); // for debug
-}
+        if (currentMillis_buzz - previousMillis_buzz >= buz_short) {
+          previousMillis_buzz = currentMillis_buzz;
+          tone(9,200,1000);
+          } 
+        }
+
+  }
